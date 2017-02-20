6ecab088ed19f54c57c96de4a141bd3d5e104e3c
Richard Heycock <rgh@filterfish.org>
2017-02-20 14:07:06 +1100

    Initial commit.

---
+++ app/Main.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DataKinds #-}
+
+module Main where
+
+import           Protolude hiding (log)
+
+import           System.IO
+import           Control.Monad.Log (Severity(..))
+import           Text.PrettyPrint.Leijen.Text (text)
+
+import qualified Logging as Log
+
+
+main :: IO ()
+main = forever $ do
+  logMessage
+  threadDelay (1000 * 1000)
+
+
+logMessage :: IO ()
+logMessage =
+  Log.withLogging Debug $ Log.log Warning (text "However, for longer running high performance applications there is a slight problem")
